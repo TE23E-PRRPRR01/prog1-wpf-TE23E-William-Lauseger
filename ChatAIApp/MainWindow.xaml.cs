bf08bb55983479809ec51f09b3c6d562b3d57bef
William Lauseger <William.larssonlauseger@elev.ga.ntig.se>
2025-03-26 15:06:33 +0100

    Tränat på nätverk och string hantering

---
+++ ChatAIApp/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Security.Cryptography.X509Certificates;
+
+
+namespace ChatAIApp;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    // Redskap för att kommunicera över http
+    HttpClient klient = new HttpClient();
+    string url = "http://10.151.168.5:11434/api/generate";
+
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void KlickSkicka(object sender, RoutedEventArgs e)
+    {
+        // Läsa av promt koden
+        string prompt = txbPrompt.Text;
+
+        // Skapa ett json-objekt
+        object data = new
+        {
+            model = "phi4:latest",
+            prompt = prompt,
+            max_tokens = 50
+        };
+
+        // Skicka till ollama-ai-servern
+        SkickaTillollama(data);
+    }
+
+    private void SkickaTillollama(object data)
+    {
+        try
+        {
+            // Skicka data till servern
+            HttpResponseMessage svar = klient.PostAsJsonAsync(url, data).Result;
+
+            // kontrollera att "requesten" lyckades
+            svar.EnsureSuccessStatusCode();
+
+            // Läs innehållet i svaret
+            string råtext = svar.Content.ReadAsStringAsync().Result;
+
+            // Dela upp råtexten i rader
+            string[] rader = råtext.Split("\n");
+
+            // gå igenom alla rader
+            foreach (string rad in rader)
+            {
+                // Finns "response
+                if (rad.Contains("response"))
+                {
+                    txbSvar.Text += PlockaUtToken(rad);
+                }
+            }
+        }
+        catch (HttpRequestException e)
+        {
+            txbSvar.Text = $"Fel: {e.Message}";
+        }
+    }
+    public string PlockaUtToken(string rad)
+    {
+        int start = rad.IndexOf("response") + 11;
+        int slut = rad.IndexOf("\"", start);
+
+        // Plocka ut token
+        return rad.Substring(start, slut - start);
+    }
+}
